ca004c604daa42378fd23ed04087f78cad3a98a1
zunda <zundan@gmail.com>
2017-10-05 16:00:56 -1000

    Add /chunked end point

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"os"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +19,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<html><body>hello</body></html>\n")
 }
 
+func handlerChunkedResponse(w http.ResponseWriter, r *http.Request) {
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		panic("expected http.ResponseWriter to be an http.Flusher")
+	}
+	for i := 1; i <= 10; i++ {
+		fmt.Fprintf(w, "Chunk #%d\n", i)
+		flusher.Flush()
+		time.Sleep(500 * time.Millisecond)
+	}
+	flusher.Flush()
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -26,6 +40,7 @@ func main() {
 
 	var httpServer http.Server
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/chunked", handlerChunkedResponse)
 	log.Println("start http listening :" + port)
 	httpServer.Addr = ":" + port
 	log.Println(httpServer.ListenAndServe())
