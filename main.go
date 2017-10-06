0c3ece3dd8e88c1389076fe2dae817022edf2a65
zunda <zundan@gmail.com>
2017-10-05 21:24:21 -1000

    Use http.NewServeMux()

--- main.go
+++ main.go
@@ -13,9 +13,18 @@ func main() {
 		port = "3000"
 	}
 
-	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from %s\n", r.URL.Path)
+	h := http.NewServeMux()
+	h.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello.\n")
 	})
+	h.HandleFunc("/404", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(404)
+		fmt.Fprint(w, "Nothing is here.\n")
+	})
+	h.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "You're at the root path.\n")
+	})
+
 	err := http.ListenAndServe(":" + port, h)
 	log.Fatal(err)
 }
