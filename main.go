618d91ddaecd014a32e88ac35c56e343bb5644d9
zunda <zundan@gmail.com>
2017-10-06 13:54:12 -1000

    Trying to implement http.ResponseWriter
    Problems:
    - WriteHeader is not called
    - http.Fluser can not be obtained

--- main.go
+++ main.go
@@ -11,13 +11,45 @@ import (
 	"time"
 )
 
+type extraHeaderResponseWriter struct {
+	origWriter http.ResponseWriter
+}
+
+func (eh *extraHeaderResponseWriter) WriteHeader(rc int) {
+	log.Println("Hey")
+	eh.origWriter.WriteHeader(rc)
+}
+
+func (eh *extraHeaderResponseWriter) Write(b []byte) (int, error) {
+	log.Println("Yo")
+	return eh.origWriter.Write(b)
+}
+
+func (eh *extraHeaderResponseWriter) Header() http.Header {
+	log.Println("Ya")
+	return eh.origWriter.Header()
+}
+
+
+func extraHeaderHandler(h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		w = &extraHeaderResponseWriter{origWriter: w}
+		h.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(fn)
+}
+
 type throttlingHandler struct {
 	d time.Duration
 }
 
 func (th *throttlingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Responding cunked lines with delays: %d\n", th.d)
-	flusher, _ := w.(http.Flusher)
+	log.Printf("Responding chunked lines with delays: %d\n", th.d)
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		log.Fatal("Could not obtain Flusher")
+	}
 
 	file, _ := os.Open("main.go")
 	defer file.Close()
@@ -28,6 +60,15 @@ func (th *throttlingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		flusher.Flush()
 		time.Sleep(th.d)
 	}
+}
+
+type bufferedHandler struct {
+}
+
+func (*bufferedHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println("Responding with buffered payload")
+	code, _ := ioutil.ReadFile("main.go")
+	fmt.Fprint(w, string(code))
 }
 
 func main() {
@@ -55,18 +96,9 @@ func main() {
 	})
 
 	h.HandleFunc("/favicon.ico", http.NotFound)
-
-	h.HandleFunc("/buf", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Responding with buffered payload")
-		code, _ := ioutil.ReadFile("main.go")
-		fmt.Fprint(w, string(code))
-	})
-
+	h.Handle("/buf", &bufferedHandler{})
 	h.Handle("/chunked", &throttlingHandler{0 * time.Millisecond})
-
 	h.Handle("/slow", &throttlingHandler{100 * time.Millisecond})
-
-	// TODO: Serve this with both Transfer-Encoding: chunked and Content-Length
 	h.HandleFunc("/mix", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Responding through IO stream with content-length")
 
@@ -74,11 +106,10 @@ func main() {
 		stat, _ := file.Stat()
 		w.Header().Set("Content-Length", strconv.FormatInt(stat.Size(), 10))
 		file.Close()
-		w.Header().Set("Transfer-Encoding", "chunked")	// This erases Content-Length
+		w.Header().Set("Transfer-Encoding", "chunked")
 
-		log.Println("Calling throttlingHandler")
-		th := &throttlingHandler{0 * time.Millisecond}
-		th.ServeHTTP(w, r)
+		log.Println("Calling extraHeaderHandler")
+		extraHeaderHandler(&bufferedHandler{}).ServeHTTP(w, r)
 	})
 
 	log.Println("Listening at port " + port)
